5c39249bb15c7d55d90db264bf1e4d54ddd00e99
Maurice Makaay <maurice@phorum.org>
2011-07-25 11:39:26 +0200

    Import of the Phorum 5.1 version of the Remember Username module.

--- remember_username.php
+++ remember_username.php
@@ -2,18 +2,18 @@
 
 if(!defined("PHORUM")) return;
 
-function mod_remember_username_after_login($data)
+function mod_remember_username_common()
 {
     $PHORUM = $GLOBALS["PHORUM"];
 
     // Remember the username for a logged in user using a cookie.
-    if ($PHORUM["DATA"]["LOGGEDIN"])
+    if ($PHORUM["DATA"]["LOGGEDIN"]) 
     {
         $username = $PHORUM["user"]["username"];
 
         // No cookie set or cookie differs? Then set a new cookie.
         if ( !isset($_COOKIE["phorum_mod_remember_username"]) ||
-             $_COOKIE["phorum_mod_remember_username"] != $username )
+             $_COOKIE["phorum_mod_remember_username"] != $username ) 
         {
             setcookie(
                 "phorum_mod_remember_username",
@@ -24,23 +24,50 @@ function mod_remember_username_after_login($data)
         }
     }
 
-    return $data;
+    // Set the username field when entering the login page. We set it
+    // here in the $_POST array, because we want the login script to set
+    // the focus to the password field. If we would simply put it
+    // in the Phorum DATA, the focus would stay on the username field.
+    // This will however trigger an error message, because a login is
+    // attempted. Therefore we set the phorum_mod_remember_username_active
+    // variable, so we can flag the after_header hook function to
+    // unset the error message.
+
+    if ( phorum_page == 'login' &&
+         ! isset($_POST["username"]) &&
+         isset($_COOKIE["phorum_mod_remember_username"]) ) {
+
+         $_POST["username"] = $_COOKIE["phorum_mod_remember_username"];
+         $_POST["password"] = "";
+         $_POST["phorum_mod_remember_username_active"] = 1;
+    }
+
+    // Because the login script will now process the login on the
+    // first entry, the temporary cookie for checking for browser
+    // cookie support will not be set. Start buffering the output,
+    // so we can set the cookie ourselves from the after_header
+    // hook function (else we get the PHP "headers already sent"
+    // error message).
+
+    ob_start();
 }
 
-function mod_remember_username_start_output()
+function mod_remember_username_after_header()
 {
-    global $PHORUM;
+    $PHORUM = $GLOBALS["PHORUM"];
 
-    // Are are entering the login page?
-    if (phorum_page == 'login' && ! isset($_POST["username"]) &&
-        isset($_COOKIE["phorum_mod_remember_username"]))
+    if (isset ($_POST["phorum_mod_remember_username_active"]))
     {
-         // Replace the username with the remembered username.
-         $PHORUM['DATA']['LOGIN']['username'] =
-             $_COOKIE["phorum_mod_remember_username"];
+        // Reset the error message when entering the login page.
+        unset($GLOBALS["PHORUM"]["DATA"]["ERROR"]);
 
-         // Make the focus shift to the password field.
-         $PHORUM['DATA']['FOCUS_TO_ID'] = 'password';
+        // Set the phorum_tmp_cookie.
+        setcookie( 
+            "phorum_tmp_cookie",
+            "this will be destroyed once logged in", 0,
+            $PHORUM["session_path"], $PHORUM["session_domain"]
+        );
+
     }
 }
 
